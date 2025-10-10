1cd0df56b86010eb0e57d227bdff182dd3e05189
Gabry <gabrielemleone@gmail.com>
2025-10-10 16:58:45 +0200

    update: bumping dependencies and fixing random sht

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@tailwindcss/vite'
 import svelte from '@astrojs/svelte'
 import icon from 'astro-icon'
 import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), svelte(), icon()],
+    vite: {
+        plugins: [tailwind()]
+    },
+    integrations: [svelte(), icon()],
     adapter: vercel({
         webAnalytics: {
             enabled: true
